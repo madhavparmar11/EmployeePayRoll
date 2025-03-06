5a22a7165cc76fd6473a1fefb6551b9221dba4e0
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-06 11:28:06 +0530

    Add files via upload

---
+++ UC5/Greeting2/src/main/java/com/Greetings2App/Greeting2/Service/GreetingService.java
@@ -0,0 +1,48 @@
+package com.Greetings2App.Greeting2.Service;
+
+import com.Greetings2App.Greeting2.Models.Greetings;
+import com.Greetings2App.Greeting2.Repository.GreetingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    private final GreetingRepository greetingRepository;
+    public GreetingService (GreetingRepository greetingRepository){
+        this.greetingRepository = greetingRepository;
+
+
+    }
+    public String getGreetingMessage() {
+
+
+        return "HelloWorld";
+}
+    //UC3
+    public String getGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName;
+        } else if (firstName != null) {
+            return "Hello " + firstName;
+        } else if (lastName != null) {
+            return "Hello " + lastName;
+        } else {
+            return "Hello World";
+}
+
+
+    }
+
+    //UC4
+    public Greetings saveGreetingMessage(String message){
+//        String Greetings=new Greetings(message);
+        return greetingRepository.save(new Greetings(message));
+}
+    public Greetings getGreetingById(Long id){
+        return greetingRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Couldn't find greeting with id"+id));
+}
+
+}
+
+
+
